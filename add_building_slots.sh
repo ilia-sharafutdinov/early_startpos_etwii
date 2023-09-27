84153beb4a022a1fab2cbf1df42d55cd9951db8c
Ilia Sharafutdinov <sharafutdinovi94@gmail.com>
2023-09-28 00:42:46 +0100

    Added building slots to script

--- add_building_slots.sh
+++ add_building_slots.sh
@@ -1,1 +1,139 @@
 #!/bin/bash
+
+# afghanistan
+python3 add_building_slot.py -u -g magistrate -b army_encampment afghanistan settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry afghanistan settlement_ordnance
+python3 add_building_slot.py -b conservatorium afghanistan settlement_culture
+
+# alsace
+python3 add_building_slot.py -u -g magistrate -b army_encampment alsace settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry alsace settlement_ordnance
+
+# belarus
+python3 add_building_slot.py -u -g magistrate -b army_encampment belarus settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry belarus settlement_ordnance
+
+# bosnia
+python3 add_building_slot.py -u -g magistrate -b army_encampment bosnia settlement_army-admin
+
+# bulgaria
+python3 add_building_slot.py -u -g magistrate -b army_encampment bulgaria settlement_army-admin
+
+# bulgaria
+python3 add_building_slot.py -u -g magistrate -b army_encampment chechenya-dagestan settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry chechenya-dagestan settlement_ordnance
+python3 add_building_slot.py -b conservatorium chechenya-dagestan settlement_culture
+
+# courland
+python3 add_building_slot.py -u -g magistrate -b army_encampment courland settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry courland settlement_ordnance
+python3 add_building_slot.py -b conservatorium courland settlement_culture
+
+# crimea
+python3 add_building_slot.py -u -g magistrate -b army_encampment crimea settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry crimea settlement_ordnance
+python3 add_building_slot.py -b conservatorium crimea settlement_culture
+
+# croatia
+python3 add_building_slot.py -u -g magistrate -b army_encampment croatia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry croatia settlement_ordnance
+
+# denmark
+python3 add_building_slot.py -b admiralty denmark settlement_navy-admin
+
+# finland
+python3 add_building_slot.py -u -g magistrate -b army_encampment finland settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry finland settlement_ordnance
+
+# flanders
+python3 add_building_slot.py -u -g magistrate -b army_encampment flanders settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry flanders settlement_ordnance
+python3 add_building_slot.py -b conservatorium flanders settlement_culture
+python3 add_building_slot.py -b admiralty flanders settlement_navy-admin
+
+# galicia
+python3 add_building_slot.py -u -g magistrate -b army_encampment galicia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry galicia settlement_ordnance
+
+# genoa
+python3 add_building_slot.py -u -g magistrate -b army_encampment genoa settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry genoa settlement_ordnance
+python3 add_building_slot.py -b conservatorium genoa settlement_culture
+
+# georgia
+python3 add_building_slot.py -u -g magistrate -b army_encampment georgia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry georgia settlement_ordnance
+python3 add_building_slot.py -b conservatorium georgia settlement_culture
+
+# hannover
+python3 add_building_slot.py -u -g magistrate -b army_encampment hannover settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry hannover settlement_ordnance
+python3 add_building_slot.py -b conservatorium hannover settlement_culture
+
+# hungary
+python3 add_building_slot.py -u -g magistrate -b army_encampment hungary settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry hungary settlement_ordnance
+
+# ireland
+python3 add_building_slot.py -u -g magistrate -b army_encampment ireland settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry ireland settlement_ordnance
+
+# lower_louisiana
+python3 add_building_slot.py -u -g magistrate -b army_encampment lower_louisiana settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry lower_louisiana settlement_ordnance
+python3 add_building_slot.py -b conservatorium lower_louisiana settlement_culture
+
+# malta
+python3 add_building_slot.py -u -g magistrate -b army_encampment malta settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry malta settlement_ordnance
+
+# milan
+python3 add_building_slot.py -u -g magistrate -b army_encampment milan settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry milan settlement_ordnance
+python3 add_building_slot.py -b conservatorium milan settlement_culture
+
+# moldavia
+python3 add_building_slot.py -u -g magistrate -b army_encampment moldavia settlement_army-admin
+
+# naples
+python3 add_building_slot.py -u -g magistrate -b army_encampment naples settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry naples settlement_ordnance
+python3 add_building_slot.py -b conservatorium naples settlement_culture
+
+# new_york
+python3 add_building_slot.py -u -g magistrate -b army_encampment new_york settlement_army-admin
+
+# norway
+python3 add_building_slot.py -u -g magistrate -b army_encampment norway settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry norway settlement_ordnance
+
+# rhineland
+python3 add_building_slot.py -u -g magistrate -b army_encampment rhineland settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry rhineland settlement_ordnance
+python3 add_building_slot.py -b conservatorium rhineland settlement_culture
+
+# savoy
+python3 add_building_slot.py -u -g magistrate -b army_encampment savoy settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry savoy settlement_ordnance
+python3 add_building_slot.py -b conservatorium savoy settlement_culture
+
+# serbia
+python3 add_building_slot.py -u -g magistrate -b army_encampment serbia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry serbia settlement_ordnance
+
+# silesia
+python3 add_building_slot.py -u -g magistrate -b army_encampment silesia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry silesia settlement_ordnance
+
+# virginia
+python3 add_building_slot.py -u -g magistrate -b army_encampment virginia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry virginia settlement_ordnance
+
+# west_prussia
+python3 add_building_slot.py -u -g magistrate -b army_encampment west_prussia settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry west_prussia settlement_ordnance
+
+# wurttemberg
+python3 add_building_slot.py -u -g magistrate -b army_encampment wurttemberg settlement_army-admin
+python3 add_building_slot.py -b cannon_foundry wurttemberg settlement_ordnance
+python3 add_building_slot.py -b conservatorium wurttemberg settlement_culture
